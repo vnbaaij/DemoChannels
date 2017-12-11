3c505378f5d599ee750eb49020e5d4f072cdbeaf
Vincent Baaij <vnbaaij@outlook.com>
2017-12-11 12:45:29 +0100

    Changed version number to follow Episerver major version

--- DemoChannels/AssemblyInfo.cs
+++ DemoChannels/AssemblyInfo.cs
@@ -2,13 +2,13 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("DemoChannels")]
-[assembly: AssemblyCopyright("Copyright ©  2017, earlier versions BVN")]
+[assembly: AssemblyCopyright("Copyright ©  2017, earlier versions BVN / The EPiCode Community")]
 [assembly: AssemblyTitle("DemoChannels")]
 [assembly: AssemblyDescription("Preview pages in an Apple Watch or Text TV context. Previously released as separate packages by The EPiCode Community and BV Network AS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Vincent Baaij")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("11.0.0.0")]
 [assembly: Guid("d531350e-1e09-4e90-ae23-6852434ddd81")]
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("11.0.0.0")]
